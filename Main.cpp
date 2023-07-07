253aa0cdcbd1d193a8ded23187059c0fb64e9cb5
Gian9911 <gianluca.giuliani1@stud.unifi.it>
2023-07-07 14:58:58 +0200

    organizing

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,41 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+
 int main()
 {
+	int w = 800;
+	int h = 800;
+	float startTime = 0.0;
+	float currentTime = 0.0;
+	float color = 0.0;
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);// using glfw 3
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);// for compatibiities etc
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OPGL", NULL, NULL);
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Lower right triangle
+		5, 4, 1 // Upper triangle
+	};
+
+	GLFWwindow* window = glfwCreateWindow(w, h, "I made this", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -20,11 +46,85 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	gladLoadGL();
+	
+
+	glViewport(0,0,w,h);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);// sennò la gpu non capisce quale shader prendere
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO;// lo uso per switchare velocemente i vbo
+	GLuint VBO;// reference for binding
+	GLuint EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);// i store vertex in vbo
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);// ???
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);//to communicate with vertex shader in outside
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	
+	// Specify the color of the background
+	glClearColor(0.77f, 0.43f, 0.27f, 1.0f);
+	// Clean the back buffer and assign the new color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+	startTime = float(glfwGetTime());
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
+		//glfwSwapBuffers(window);  
+		currentTime = float(glfwGetTime());
+		if (currentTime - startTime >= 1.0)
+		{
+			startTime = currentTime;
+			currentTime += 0.1;
+			color += 0.1;
+			glClearColor(float(sin(color)), float(cos(color)), float(tan(color)), 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
