e3aacc8ecac7f0c008e6da13c63cc6ea44119e6a
Gian9911 <gianluca.giuliani1@stud.unifi.it>
2023-07-07 09:30:37 +0200

    window cretion

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,28 @@
 
 int main()
 {
+	glfwInit();
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	int g = 0;
-	int gg = 0;
-	float f = gg;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);// using glfw 3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);// for compatibiities etc
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OPGL", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
